ea01b21aecb64210195fe46ce2b84a76c6de0ba9
Kajetan Szczypiński <kajetan.szczypinski@cyfroweszkoly.pl>
2026-04-10 22:41:20 +0200

    add data class transaction

--- app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
+++ app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
@@ -1,2 +1,10 @@
 package com.example.wokolskidashboard.model
 
+data class Transaction(
+    val id: Int = (0..1000000).random(),
+    val name: String,
+    val amount: Double,
+    val isExpense: Boolean,
+    val category: String,
+    val isNecessary: Boolean = true
+)
