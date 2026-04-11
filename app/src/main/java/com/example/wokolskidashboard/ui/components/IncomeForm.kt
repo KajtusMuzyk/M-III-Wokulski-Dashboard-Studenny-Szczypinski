cb5132d7215a74789458c3bd6b970c6b775eb943
Studi <jakub.studenny@cyfroweszkoly.pl>
2026-04-11 14:49:45 +0200

    Dodanie funkcji, zmiennych name i amount i 2 textfieldow

--- app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
@@ -1,2 +1,22 @@
 package com.example.wokolskidashboard.ui.components
 
+@Composable
+fun IncomeForm(onAddIncome: (Transaction) -> Unit) {
+    var name by remember { mutableStateOf("") }
+    var amount by remember { mutableStateOf("") }
+
+
+
+    TextField(
+        value = name,
+        onValueChange = { name = it },
+        label = { Text("Nazwa towaru") }
+    )
+
+    TextField(
+        value = amount,
+        onValueChange = { amount = it },
+        label = { Text("Kwota (ruble)") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+}
